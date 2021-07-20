37fcb5613ca3b09655f1395c7708258823490d76
Jorge Alegretti <jorgeluiz.b.alegretti@gmail.com>
2021-07-20 17:39:46 -0300

    feat: implemented verification password end generate token

--- src/modules/accounts/useCases/authenticateUser/AuthenticateUserUseCase.js
+++ src/modules/accounts/useCases/authenticateUser/AuthenticateUserUseCase.js
@@ -1,13 +1,28 @@
+import jsonwebtoken from 'jsonwebtoken';
+
+import auth from '../../../../config/authentication.js';
+
 class AuthenticateUserUseCase {
-  constructor(userTokenRepository) {
+  constructor(userTokenRepository, userRepository) {
     this.userTokenRepository = userTokenRepository;
+    this.userRepository = userRepository;
   }
 
   async execute({ email, password }) {
-    const token = await this.userTokenRepository.authenticate({
-      email,
-      password,
+    const user = await this.userRepository.findOneByEmail(email);
+
+    const passwordMatch = user.password === password;
+
+    if (!passwordMatch) {
+      throw new Error('Email or password incorrect!');
+    }
+
+    const { expires_in_token, secret_token } = auth;
+
+    const token = jsonwebtoken.sign({ email }, secret_token, {
+      expiresIn: expires_in_token,
     });
+
     return token;
   }
 }

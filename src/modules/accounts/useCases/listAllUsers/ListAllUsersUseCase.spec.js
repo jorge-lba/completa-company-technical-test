46e7b758845eba7b1285baa469d07707bd6f97ac
Jorge Alegretti <jorgeluiz.b.alegretti@gmail.com>
2021-07-19 23:23:23 -0300

    test: create a new user

--- src/modules/accounts/useCases/listAllUsers/ListAllUsersUseCase.spec.js
+++ src/modules/accounts/useCases/listAllUsers/ListAllUsersUseCase.spec.js
@@ -1,16 +1,25 @@
 import { UserRepositoryImpInMemory } from '../../repositories/in-memory/UserRepositoryImpInMemory.js';
+import { ListAllUsersUseCase } from './ListAllUsersUseCase.js';
 
 describe('List all Uses use case', () => {
   let userRepository;
-  let useCase;
+  let listAllUsersUseCase;
 
   beforeEach(() => {
     userRepository = UserRepositoryImpInMemory.getInstance();
-    useCase = ListAllUsersUseCase(userRepository);
+    listAllUsersUseCase = new ListAllUsersUseCase(userRepository);
   });
 
   it('should list all users', async () => {
-    const response = await ListAllUsersUseCase.execute();
+    const user = {
+      name: 'testName',
+      email: 'user@test.com',
+      password: 'testPassword',
+    };
+
+    await userRepository.create(user);
+
+    const response = await listAllUsersUseCase.execute();
     expect(response.length).toBeGreaterThan(0);
   });
 });

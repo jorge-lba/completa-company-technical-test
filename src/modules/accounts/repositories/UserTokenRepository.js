92f8d5fe3b3b5cb05048d67028dc7df810c081ea
Jorge Alegretti <jorgeluiz.b.alegretti@gmail.com>
2021-07-20 16:39:50 -0300

    feat: implemented abstract class user token repository

---
+++ src/modules/accounts/repositories/UserTokenRepository.js
@@ -0,0 +1,17 @@
+class UserTokenRepository {
+  constructor(repository) {
+    this.repository = repository;
+
+    if(this.constrictor === UserTokenRepository) {
+      throw new TypeError("Cannot construct Abstract instances directly");
+    }
+  }
+
+  static getInstance() {
+    if(!this.instance) {
+      this.instance = new this();
+    }
+    return this.instance;
+  }
+}
+

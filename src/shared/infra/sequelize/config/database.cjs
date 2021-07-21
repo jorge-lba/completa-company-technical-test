5073247845a917d9067e6ec168b05be92c0738e0
Jorge Alegretti <jorgeluiz.b.alegretti@gmail.com>
2021-07-21 02:02:48 -0300

    feat: config sequelize run migrations

---
+++ src/shared/infra/sequelize/config/database.cjs
@@ -0,0 +1,15 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+require('babel-core/register');
+
+module.exports = {
+  dialect: 'postgres',
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  define: {
+    timestamps: true,
+    underscore: true,
+  },
+};

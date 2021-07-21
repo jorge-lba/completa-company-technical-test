8f63a60e2f68fcc91da4ba458d64328f62dc2053
Jorge Alegretti <jorgeluiz.b.alegretti@gmail.com>
2021-07-21 02:03:30 -0300

    feat: create migration users

---
+++ src/shared/infra/sequelize/migrations/20210721034344-create-users.js
@@ -0,0 +1,33 @@
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('users', {
+      id: {
+        allowNull: false,
+        primaryKey: true,
+        defaultValue: Sequelize.UUIDV4,
+        type: Sequelize.UUID,
+      },
+      name: {
+        type: Sequelize.STRING,
+      },
+      email: {
+        type: Sequelize.STRING,
+      },
+      password: {
+        type: Sequelize.STRING,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+
+  down: async (queryInterface) => {
+    await queryInterface.dropTable('users');
+  },
+};

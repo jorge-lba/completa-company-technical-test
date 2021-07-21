6bdd75a47069b978524ee6ceb4d3352dfce0191e
Jorge Alegretti <jorgeluiz.b.alegretti@gmail.com>
2021-07-20 21:24:15 -0300

    test: must contain a token and a refresh token

--- src/modules/accounts/useCases/authenticateUser/AutenticateUserUseCase.spec.js
+++ src/modules/accounts/useCases/authenticateUser/AutenticateUserUseCase.spec.js
@@ -1,5 +1,9 @@
+import jwt from 'jsonwebtoken';
+
 import { UserRepositoryImpInMemory } from '../../repositories/in-memory/UserRepositoryImpInMemory.js';
 import { AuthenticateUserUseCase } from './AuthenticateUserUseCase.js';
+
+import auth from '../../../../config/authentication.js';
 
 describe('Authenticate user Use Case', () => {
   let userRepository;
@@ -25,5 +29,36 @@ describe('Authenticate user Use Case', () => {
     });
 
     expect(token).toBeTruthy();
+  });
+
+  it(`must contain a token and a refresh token`, async () => {
+    const user = {
+      name: 'userRefreshToken',
+      email: 'refreshToken@test.com',
+      password: 'testRefreshToken',
+    };
+
+    const { id: userId } = await userRepository.create(user);
+
+    const { token, refreshToken } = await authenticateUserUseCase.execute({
+      email: user.email,
+      password: user.password,
+    });
+
+    const tokenIsValid = jwt.verify(token, auth.secret_token);
+    const refreshTokenIsValid = jwt.verify(
+      refreshToken,
+      auth.secret_refresh_token
+    );
+
+    console.log(tokenIsValid);
+    console.log(refreshTokenIsValid);
+
+    expect(token).toBeTruthy();
+    expect(tokenIsValid.subject).toBe(userId);
+
+    expect(refreshToken).toBeTruthy();
+    expect(refreshTokenIsValid.subject).toBe(userId);
+    expect(refreshTokenIsValid.email).toBe(user.email);
   });
 });

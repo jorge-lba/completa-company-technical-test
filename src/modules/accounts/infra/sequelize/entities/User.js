486b25ed1bb3f356db13ac81eab5560c22648e3b
Jorge Alegretti <jorgeluiz.b.alegretti@gmail.com>
2021-07-21 02:39:33 -0300

    feat: create user entitie

---
+++ src/modules/accounts/infra/sequelize/entities/User.js
@@ -0,0 +1,20 @@
+import Sequelize from 'sequelize';
+
+const { Model, DataTypes } = Sequelize;
+
+class User extends Model {
+  static init(sequelize) {
+    super.init(
+      {
+        name: DataTypes.STRING,
+        email: DataTypes.STRING,
+        password: DataTypes.STRING,
+      },
+      {
+        sequelize,
+      }
+    );
+  }
+}
+
+export { User };
